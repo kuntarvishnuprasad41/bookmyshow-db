e43f007288cde01a51ee5508ee639bb0d6c4aaa1
Vishnuprasad Kuntar <kuntarvishnuprasad41@gmail.com>
2023-11-12 11:35:32 +0530

    TABLE FOR USERS AND MOVIES

--- users.sql
+++ users.sql
@@ -3,11 +3,29 @@ CREATE DATABASE book_my_show;
 use book_my_show;
 
 CREATE TABLE users(
-    id BIGINT(20) NOT NULL AUTO_INCREMENT,
+    id UUID NOT NULL,
     mobile varchar(13),
     email varchar(50),
     first_name varchar(20),
     middle_name varchar(20),
     last_name varchar(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     primary key (id)
 );
+
+CREATE TABLE movies(
+    id uuid NOT NULL,
+    censor_certificate_type varchar(5) NOT NULL,
+    language varchar(20) NOT NULL,
+    name varchar(20) NOT NULL,
+    release_date varchar(20) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    primary key(id)
+);
+
+
+
+
+
