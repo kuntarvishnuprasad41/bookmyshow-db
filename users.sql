95ab35c1f2010cf36d7bedc5c413878d513d9b1b
Vishnuprasad Kuntar <kuntarvishnuprasad41@gmail.com>
2023-11-12 11:43:12 +0530

    TABLE FOR TICKETS BOOKED

--- users.sql
+++ users.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE book_my_show;
+# CREATE DATABASE book_my_show;
 
 use book_my_show;
 
 CREATE TABLE users(
-    id UUID NOT NULL,
+    id BIGINT(20) NOT NULL,
     mobile varchar(13),
     email varchar(50),
     first_name varchar(20),
@@ -15,7 +15,7 @@ CREATE TABLE users(
 );
 
 CREATE TABLE movies(
-    id uuid NOT NULL,
+    id BIGINT(20) NOT NULL,
     censor_certificate_type varchar(5) NOT NULL,
     language varchar(20) NOT NULL,
     name varchar(20) NOT NULL,
@@ -26,6 +26,18 @@ CREATE TABLE movies(
 );
 
 
-
+CREATE TABLE tickets(
+    id BIGINT(20) NOT NULL,
+    seats_allotted varchar(255) NOT NULL,
+    amount DOUBLE NOT NULL ,
+    booked_at DATETIME NOT NULL,
+    movie_id BIGINT(20) NOT NULL ,
+    user_id BIGINT(20) NOT NULL ,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    primary key (id),
+    CONSTRAINT movie_id FOREIGN KEY (movie_id) REFERENCES movies(id),
+    CONSTRAINT user_id FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
 
